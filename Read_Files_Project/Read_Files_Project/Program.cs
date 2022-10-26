95a145cd65c6f8e74c097915545fce089e83aaf8
kishore <Kishore.Lakkireddy@ivycomptech.com>
2022-10-26 13:29:43 +0530

    Initial commit

---
+++ Read_Files_Project/Read_Files_Project/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Read_Files_Project
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string files1 = "C:\\Users\\kishore.lakkireddy\\OneDrive - Entain Group\\files1.txt";
+            Console.WriteLine(File.ReadAllText(files1));
+            string files2 = "C:\\Users\\kishore.lakkireddy\\OneDrive - Entain Group\\files2.txt";
+            await Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine(File.ReadAllText(files2));
+            });
+        }
+    }
+}
---
+++ Swap_Files_project/Swap_Files_project/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Swap_Files_project
+{
+   
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+
+                string path = "C:\\Users\\kishore.lakkireddy\\OneDrive - Entain Group\\FolderFirst";
+                Directory.CreateDirectory(path);
+                FileStream fs = new FileStream(path + "\\file1.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.WriteLine("Hello Everyone,\n This is File1 Contents");
+                sw.Close();
+                fs.Close();
+                path = "C:\\Users\\kishore.lakkireddy\\OneDrive - Entain Group\\FolderSecond";
+                Directory.CreateDirectory(path);
+                fs = new FileStream(path + "\\file2.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                sw = new StreamWriter(fs);
+                sw.WriteLine("Hello Everyone,\n This is File2 Contents");
+                sw.Close();
+                fs.Close();
+                File.Move("C:\\Users\\kishore.lakkireddy\\OneDrive - Entain Group\\FolderFirst\\file1.txt", "C:\\Users\\kishore.lakkireddy\\OneDrive - Entain Group\\FolderSecond\\file1.txt");
+                File.Delete("C:\\Users\\kishore.lakkireddy\\OneDrive - Entain Group\\FolderFirst\\file1.txt");
+                File.Move("C:\\Users\\kishore.lakkireddy\\OneDrive - Entain Group\\FolderSecond\\file2.txt", "C:\\Users\\kishore.lakkireddy\\OneDrive - Entain Group\\FolderFirst\\file2.txt");
+                File.Delete("C:\\Users\\kishore.lakkireddy\\OneDrive - Entain Group\\FolderSecond\\file2.txt");
+
+            }
+        }
+    }
+
